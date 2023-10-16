67ae2decb28df624d2924fa6e7b4995cb54debab
Joakim Falch <joakimfalch@outlook.com>
2023-10-16 23:50:41 +0200

    Fixed water droplet filling

--- App.js
+++ App.js
@@ -5,45 +5,49 @@ import MainScreen from './screens/MainScreen';
 import StatisticsScreen from './screens/StatisticsScreen';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
+import globalStyles from "./assets/globalStyles";
+import {View} from "react-native";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <Tab.Navigator
-            initialRouteName="Main"
-            screenOptions={{
-                tabBarActiveTintColor: 'blue',
-                tabBarInactiveTintColor: 'gray',
-                tabBarStyle: [
-                    {
-                        display: 'flex'
-                    },
-                    null
-                ]
-            }}>
-        <Tab.Screen
-            name="Main"
-            component={MainScreen}
-            options={{
-                tabBarIcon: ({ color, size }) => (
-                    <FontAwesome5 name="home" color={color} size={size} />
-                )
+      <View style={globalStyles.appBackground}>
+          <NavigationContainer>
+            <Tab.Navigator
+                initialRouteName="Main"
+                screenOptions={{
+                    tabBarActiveTintColor: 'blue',
+                    tabBarInactiveTintColor: 'gray',
+                    tabBarStyle: [
+                        {
+                            display: 'flex'
+                        },
+                        null
+                    ]
+                }}>
+            <Tab.Screen
+                name="Main"
+                component={MainScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome5 name="home" color={color} size={size} />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Statistics"
+                component={StatisticsScreen}
+                options={{
+                    tabBarLabel: 'Statistics',
+                    tabBarIcon: ({ color, size}) => (
+                        <Ionicons name="stats-chart" color={color} size={size}/>
+                    )
             }}
-        />
-        <Tab.Screen
-            name="Statistics"
-            component={StatisticsScreen}
-            options={{
-                tabBarLabel: 'Statistics',
-                tabBarIcon: ({ color, size}) => (
-                    <Ionicons name="stats-chart" color={color} size={size}/>
-                )
-        }}
-        />
-      </Tab.Navigator>
-      </NavigationContainer>
+            />
+          </Tab.Navigator>
+          </NavigationContainer>
+      </View>
   );
 }
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import globalStyles from "../assets/globalStyles";
 
 function MainScreen() {
     const [waterIntake, setWaterIntake] = useState(0);
@@ -14,24 +15,27 @@ function MainScreen() {
 
     const fillPercentage = (waterIntake / 2000) * 100; // Assuming 2L is 100%
 
-    const gradientTransparency = 1 - (fillPercentage / 100);
+    const emptySpace = 300 - (3 * fillPercentage);
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.header}>Today</Text>
+        <View style={globalStyles.appBackground}>
+            <View style={styles.container}>
+                <Text style={styles.header}>Today</Text>
 
-            <View style={styles.imageContainer}>
-                <Image source={require('../assets/droplet.png')} style={styles.droplet} />
-                <Image source={require('../assets/filledDroplet.png')} style={styles.filledDroplet} />
+                <View style={styles.imageContainer}>
+                    <Image source={require('../assets/droplet.png')} style={styles.droplet} />
+                    <Image source={require('../assets/filledDroplet.png')} style={styles.filledDroplet} />
+                    <View style={{ position: 'absolute', top: 0, left: 0, right: 0, height: emptySpace, backgroundColor: 'white' }} />
+                </View>
+
+                <TouchableOpacity style={styles.button} onPress={addGlass}>
+                    <Text>Add a Glass</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button} onPress={addCustomAmount}>
+                    <Text>Add Custom Amount</Text>
+                </TouchableOpacity>
             </View>
-
-            <TouchableOpacity style={styles.button} onPress={addGlass}>
-                <Text>Add a Glass</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button} onPress={addCustomAmount}>
-                <Text>Add Custom Amount</Text>
-            </TouchableOpacity>
         </View>
     );
 }
